46b63479d4c14fe80e320990ce2150a76e3624f0
skyazid <hassaine.yazid.univ@gmail.com>
2018-06-23 16:00:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+### Spring boot application + ngx-admin
+
+> Requirements 
+Install NodeJS
+Copy ngx-admin or other angular project inside "src\main\angular"
+Run "mvn clean install"
---
+++ src/main/java/com/demo/ngxadmin/NgxAdminApplication.java
@@ -0,0 +1,12 @@
+package com.demo.ngxadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgxAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NgxAdminApplication.class, args);
+    }
+}
